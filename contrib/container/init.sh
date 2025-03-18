486aa132e569c4f87c4cd463ccb206bdc2c2821b
Joshua Mathianas <mathianasj@gmail.com>
2025-03-18 16:54:10 -0400

    fix init

--- contrib/container/init.sh
+++ contrib/container/init.sh
@@ -5,7 +5,7 @@ set -e
 
 # Required to suppress some git errors further down the line
 if command -v git &> /dev/null; then
-    git config --global --add safe.directory /home/***
+    git config --global --add safe.directory "/home/***"
 fi
 
 # Create required directory structure (if it does not already exist)
@@ -40,17 +40,14 @@ if [[ -n "$INVENTREE_PY_ENV" ]]; then
     if test -d "$INVENTREE_PY_ENV"; then
         # venv already exists
         echo "Using Python virtual environment: ${INVENTREE_PY_ENV}"
-        source ${INVENTREE_PY_ENV}/bin/activate
     else
-        # Setup a virtual environment (within the provided directory)
+        # Setup a virtual environment (within the "data/env" directory)
         echo "Running first time setup for python environment"
         python3 -m venv ${INVENTREE_PY_ENV} --system-site-packages --upgrade-deps
-
-        # Ensure invoke tool is installed locally
-        source ${INVENTREE_PY_ENV}/bin/activate
-        python3 -m pip install --ignore-installed --upgrade invoke
     fi
 
+    # Now activate the venv
+    source ${INVENTREE_PY_ENV}/bin/activate
 fi
 
 cd ${INVENTREE_HOME}
